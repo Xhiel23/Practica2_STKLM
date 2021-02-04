de2dd1ebafafdaf2f1b645d9d6fbc9ef2350aa9d
Beatriz Cabrero <b.cabrero23@gmail.com>
2021-02-04 10:37:08 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,49 @@
-// const gulp = require('gulp');
-const { src, dest, watch, series } = require('gulp');
-const browsersync = require('browser-sync').create();
-// Project Set Up
+const fs = require("fs");
+const { src, dest, watch, series } = require("gulp");
+const browsersync = require("browser-sync").create();
 
-// const fs = require('fs');
-// gulp.task('directories', () => {
-//     const folders = [
-//         // 'app',
-//         // 'dist',
-//         'css',
-//         'images',
-//         'js',
-//         'scss',
-//     ];
-//     folders.forEach(dir => {
-//         if(!fs.existsSync(dir)) {
-//             fs.mkdirSync(dir);
-//             console.log('The following folder was created:', dir);    
-//         }   
-//     });
-// });
+// Project Structure
+function makeStructure() {
+  const folders = ["app", "dist", "css", "images", "js", "scss"];
+  folders.forEach(dir => {
+    if(!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+        console.log('The following folder was created:', dir);    
+      }   
+    });
+}
+exports.makeStructure = makeStructure;
 
 // SASS Tasks
 
-const sass = require('gulp-sass');
+const sass = require("gulp-sass");
 
-function scssTask(){
-    return src('scss/styles.scss', { sourcemaps: true })
-      .pipe(sass())
-      .pipe(dest('css', { sourcemaps: '.' }));
-  }
+function scssTask() {
+  return src("scss/styles.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(dest("css", { sourcemaps: "." }));
+}
 
- // Live Server
- function browsersyncServe(cb){
-    browsersync.init({
-      server: {
-        baseDir: '.'
-      }
-    });
-    cb();
-  } 
+// Live Server
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
+}
 
-  function browsersyncReload(cb){
-    browsersync.reload();
-    cb();
-  }
-  
-  // Watch Task
-  function watchTask(){
-    watch('*.html', browsersyncReload);
-    watch(['scss/**/*.scss', 'js/**/*.js'], series(scssTask, browsersyncReload));
-  }
-  
-  // Default Gulp task
-  exports.default = series(
-    scssTask,
-    browsersyncServe,
-    watchTask
-  );
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
 
+// Watch Task
+function watchTask() {
+  watch("*.html", browsersyncReload);
+  watch(["scss/**/*.scss", "js/**/*.js"], series(scssTask, browsersyncReload));
+}
+
+// Default Gulp task
+exports.default = series(scssTask, browsersyncServe, watchTask);
